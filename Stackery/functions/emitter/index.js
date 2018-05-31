8b15dcd6621fec77f790d58bb5d4eb2d2c9f56f1
tobyfee@stackery.io <tobyfee@stackery.io>
2018-05-31 20:02:45 +0000

    Modified 1 node:
    Added function node emitter

---
+++ Stackery/functions/emitter/index.js
@@ -0,0 +1,24 @@
+module.exports = async message => {
+  console.dir(message);
+
+  return {
+"default": "edgar", 
+"email": "edgar", 
+"sqs": "edgar", 
+"lambda": "edgar", 
+"http": "edgar", 
+"https": "edgar", 
+"sms": "edgar", 
+"APNS": "{\"aps\":{\"alert\": \"edgar\"} }", 
+"APNS_SANDBOX":"{\"aps\":{\"alert\":\"edgar\"}}", 
+"APNS_VOIP":"{\"aps\":{\"alert\":\"edgar\"}}", 
+"APNS_VOIP_SANDBOX": "{\"aps\":{\"alert\": \"edgar\"} }", 
+"MACOS":"{\"aps\":{\"alert\":\"edgar\"}}", 
+"MACOS_SANDBOX": "{\"aps\":{\"alert\": \"edgar\"} }", 
+"GCM": "{ \"data\": { \"message\": \"edgar\" } }", 
+"ADM": "{ \"data\": { \"message\": \"edgar\" } }", 
+"BAIDU": "{\"title\":\"edgar\",\"description\":\"edgar\"}", 
+"MPNS" : "<?xml version=\"1.0\" encoding=\"utf-8\"?><wp:Notification xmlns:wp=\"WPNotification\"><wp:Tile><wp:Count>ENTER COUNT</wp:Count><wp:Title>edgar</wp:Title></wp:Tile></wp:Notification>", 
+"WNS" : "<badge version=\"1\" value=\"23\"/>"
+};
+}
